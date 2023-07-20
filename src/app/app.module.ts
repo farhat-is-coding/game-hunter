3c7193d7b8555c0505c8d6d1366459ae63d4a815
farhat-is-coding <farhxt.abbxs69@gmail.com>
2023-07-21 02:01:32 +0500

    game component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { GameComponent } from './home/game/game.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
